ee8dd6d5a3a7844baa769aea9e2d9d52adfd10f5
Edmund Wohlmuth <edmundwoh@gmail.com>
2022-03-23 12:05:43 -0300

    Added enemy movement

--- Assets/Scripts/enemyMovement.cs
+++ Assets/Scripts/enemyMovement.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemyMovement : MonoBehaviour
 {
+    public NavMeshAgent enemy;
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class enemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        enemy.SetDestination(player.transform.position);
     }
 }
